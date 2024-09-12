209a268a37f24709db263914fd16d96f5e278b0d
unknown <dixitgarima24@gmail.com>
2024-09-12 17:43:06 +0530

    show

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Balance from './Balance'
+// import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Balance/>
+    {/* <Home/> */}
   </React.StrictMode>
 );
 
